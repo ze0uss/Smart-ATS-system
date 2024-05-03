bda5b9431a90f4c35efb7ab8c38fc876a6fb076d
ZEUS <114276911+ze0uss@users.noreply.github.com>
2024-05-03 15:23:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 
 load_dotenv() ## load all our environment variables
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("AIzaSyDbNAJf6fqsCxnIw-UuN2K8Kfx2ADcGCCI"))
 
 def get_gemini_repsonse(input):
     model=genai.GenerativeModel('gemini-pro')
